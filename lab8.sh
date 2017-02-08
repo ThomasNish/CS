a1d72df3fc31d0b2c9740645441d21dbe4c373ce
Thomas Nishimura <tn44@evansville.edu>
2017-02-08 11:34:29 -0800

    Getting lab8 in a rough working order

---
+++ lab8.sh
@@ -0,0 +1,81 @@
+#!/bin/bash
+
+. /root/Projects/CS126-Projects/functionlibrary.sh
+trap trapforcrtc SIGINT
+
+echo "Please enter your desired IP address"
+read IPADDRESS
+
+if [[ -z $IPADDRESS ]]
+then 
+{
+	echo "You did not enter an IP address"
+	exit 1
+}	 
+else 
+	check_ip $IPADDRESS
+fi
+
+echo "Please enter your desired Netmask"
+read NETMASK
+
+if [[ -z $NETMASK ]]
+then 
+{
+	echo "You did not enter a Netmask"
+	exit 1	
+}
+else 
+	check_ip $NETMASK
+fi
+
+echo "What Nameserver would you like to use?"
+read NAMESERVER
+
+if [[ -z $NAMESERVER ]]
+then
+{
+	echo "You did not enter a Nameserver"
+	exit 1
+}
+fi
+
+echo "What is your desired default gateway?"
+read GATEWAY
+
+if [[ -z $GATEWAY ]]
+then
+{ 
+	echo "You did not enter a Gateway Address"
+	exit 1
+}
+else
+	check_ip $GATEWAY
+fi
+
+ifconfig eth1 $IPADDRESS
+ifconfig eth1 netmask $NETMASK
+
+grep -v "$NAMESERVER" /etc/resolv.conf > tmp.resolv.conf
+echo "nameserver" $NAMESERVER >> /tmp/resolv.conf
+#cat /tmp/resolv/conf > /etc/resolv.conf
+
+
+touch /tmp/ifcfg-eth1
+
+  echo "# Created by Netwiz" >           /tmp/ifcfg-eth1
+  echo 'NAME="eth1"'	     >>		 /tmp/ifcfg-eth1
+  echo "ONBOOT=yes" 	     >>		 /tmp/ifcfg-eth1
+  echo "NETBOOT=yes"	     >>		 /tmp/ifcfg-eth1
+  echo "IPADDR="$IPADDRESS   >>		 /tmp/ifcfg-eth1
+  echo "NETMASK="$NETMASK    >>		 /tmp/ifcfg-eth1
+  echo "IPV6INIT=yes"	     >>		 /tmp/ifcfg-eth1
+  echo "BOOTPROTO=none"      >>		 /tmp/ifcfg-eth1
+  echo "TYPE=Ethernet"	     >>		 /tmp/ifcfg-eth1
+  echo "DNS1="$NAMESERVER    >>          /tmp/ifcfg-eth1
+  echo 'NM_CONTROLLED="no"'  >>		 /tmp/ifcfg-eth1
+
+#mv /tmp/ifcfg-eth1 /etc/sysconfig/network-scripts
+
+
+#cleanup
--- salt/crypttab.sls
+++ salt/crypttab.sls
@@ -1,4 +1,4 @@
   crypttab:
     file.managed:
       - name: /etc/crypttab
-      - source: /root/Projects/CS126-Projects/Crypttab 
+      - source: salt://centos/7/crypttab
